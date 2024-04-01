5e0097e9f8937e74fc1a1275e2cf74cd767318e4
sunoc <sunoc@debian>
2024-04-01 15:31:55 +0900

    add a very minimal configure script

---
+++ config/freertos/kria_kr260/configure.sh
@@ -0,0 +1,5 @@
+EXTENSIONS_DIR=$FW_TARGETDIR/freertos_apps/microros_nucleo_f446ze_extensions
+
+echo "Very little configuration is needed for the KRIA KR260 board."
+echo "Saving the app name."
+echo $CONFIG_NAME > $FW_TARGETDIR/APP
